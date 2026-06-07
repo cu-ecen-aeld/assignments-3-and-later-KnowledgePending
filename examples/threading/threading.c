d9bef5c514c3a1b3a7e72cb1d9fe429c2c05d0b4
KnowledgePending <bryanfloodmain@gmail.com>
2026-06-07 11:04:56 +0100

    feat: add threading and sync impls

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,10 +10,17 @@
 
 void* threadfunc(void* thread_param)
 {
+    struct thread_data *args = (struct thread_data *) thread_param;
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    usleep(args->wait_to_obtain_ms * 1000);
+
+    pthread_mutex_lock(args->mutex);
+
+    usleep(args->wait_to_release_ms * 1000);
+
+    pthread_mutex_unlock(args->mutex);
+    args->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +35,21 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
 
+    struct thread_data *data = malloc(sizeof(struct thread_data));
+    if (!data)
+        return false;
+
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+
+    int rc = pthread_create(thread, NULL, threadfunc, data);
+    if (rc != 0)
+    {
+        free(data);
+        return false;
+    }
+
+    return true;
+}

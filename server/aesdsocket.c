2498ff950dbe1311c8b74fe9efbd7ae00a89fdf7
KnowledgePending <bryanfloodmain@gmail.com>
2026-06-13 10:04:36 +0100

    feat: add writting recieved data to file

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -9,17 +9,24 @@
 #include <syslog.h>
 
 #define SERVER_PORT 9000
-#define BUFFER_LEN 1024
+#define READ_BUF_SIZE 1024
+#define ACCUM_BUF_SIZE 8192
 
 int main(void)
 {
-    openlog("writer", LOG_PID, LOG_USER);
+    openlog("aesdsocket", LOG_PID, LOG_USER);
+    FILE *fp = fopen("/var/tmp/aesdsocketdata", "ab+");
+
+    if (fp == NULL)
+    {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
     int server_fd, new_socket;
-    ssize_t valread;
     struct sockaddr_in address;
     socklen_t addrlen = sizeof(address);
     int opt = 1;
-    char buffer[BUFFER_LEN];
 
     memset(&address, 0, sizeof(address));
 
@@ -58,8 +65,7 @@ int main(void)
     new_socket = accept(
         server_fd,
         (struct sockaddr *)&address,
-        &addrlen
-    );
+        &addrlen);
 
     if (new_socket < 0)
     {
@@ -67,30 +73,82 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    valread = read(new_socket, buffer, BUFFER_LEN - 1);
-
-    if (valread < 0)
-    {
-        perror("read failed");
-        exit(EXIT_FAILURE);
-    }
-
-    buffer[valread] = '\0';
-
-    printf("message received: %s\n", buffer);
     char client_ip[INET_ADDRSTRLEN];
 
     inet_ntop(AF_INET,
-            &address.sin_addr,
-            client_ip,
-            sizeof(client_ip));
+              &address.sin_addr,
+              client_ip,
+              sizeof(client_ip));
 
     syslog(LOG_DEBUG,
-        "Accepted connection from \"%s\"",
-        client_ip);
+           "Accepted connection from \"%s\"",
+           client_ip);
+
+    // recieving data
+    char read_buf[READ_BUF_SIZE];
+    char accum[ACCUM_BUF_SIZE];
+    size_t accum_len = 0;
+
+    for (;;)
+    {
+        ssize_t n = read(new_socket,
+                         read_buf,
+                         sizeof(read_buf));
+
+        if (n < 0)
+        {
+            perror("read");
+            break;
+        }
+
+        if (n == 0)
+        {
+            /* client disconnected */
+            break;
+        }
+
+        if (accum_len + n > sizeof(accum))
+        {
+            fprintf(stderr, "message too large\n");
+            break;
+        }
+
+        memcpy(accum + accum_len, read_buf, n);
+        accum_len += n;
+
+        char *line_start = accum;
+        char *newline;
+
+        while ((newline = memchr(line_start,
+                                 '\n',
+                                 accum + accum_len - line_start)) != NULL)
+        {
+            size_t line_len = newline - line_start + 1;
+
+            fwrite(line_start, 1, line_len, fp);
+            fflush(fp);
+
+            line_start = newline + 1;
+        }
+
+        size_t remaining = accum + accum_len - line_start;
+
+        memmove(accum, line_start, remaining);
+        accum_len = remaining;
+    }
+
+    if (accum_len > 0)
+    {
+        fwrite(accum, 1, accum_len, fp);
+    }
 
     close(new_socket);
     close(server_fd);
+    syslog(LOG_DEBUG,
+           "Closed connection from \"%s\"",
+           client_ip);
+
+    fclose(fp);
     closelog();
 
     return 0;
